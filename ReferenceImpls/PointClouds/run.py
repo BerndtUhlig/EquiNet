6ae4b8851360c0d3e6930f130909ef55a5265313
BerndtUhlig <berndt.uhlig@libertari.at>
2025-07-25 18:39:16 +0200

    Pushed version for a size of 5000

--- ReferenceImpls/PointClouds/run.py
+++ ReferenceImpls/PointClouds/run.py
@@ -12,8 +12,8 @@ import modelnet
 #################### Settings ##############################
 num_epochs = 1000
 batch_size = 64
-downsample = 10    #For 5000 points use 2, for 1000 use 10, for 100 use 100
-network_dim = 256  #For 5000 points use 512, for 1000 use 256, for 100 use 256
+downsample = 2    #For 5000 points use 2, for 1000 use 10, for 100 use 100
+network_dim = 512  #For 5000 points use 512, for 1000 use 256, for 100 use 256
 num_repeats = 5    #Number of times to repeat the experiment
 data_path = 'ModelNet40_cloud.h5'
 #################### Settings ##############################
@@ -28,11 +28,11 @@ class PointCloudTrainer(object):
         self.model_fetcher = modelnet.ModelFetcher(data_path, batch_size, downsample, do_standardize=True, do_augmentation=True)
         sample_size = int(10000/downsample)
         #Setup network
-        self.D = classifier.DPCSTanh(sample_size,network_dim, pool='mean').cuda()
+        self.D = classifier.DPCSTanh(sample_size,network_dim, pool='max').cuda()
         self.L = nn.CrossEntropyLoss().cuda()
         self.optimizer = optim.Adam([{'params':self.D.parameters()}], lr=1e-3, weight_decay=1e-7, eps=1e-3)
-        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=list(range(400,num_epochs,400)), gamma=0.1)
-        #self.optimizer = optim.Adamax([{'params':self.D.parameters()}], lr=5e-4, weight_decay=1e-7, eps=1e-3) # optionally use this for 5000 points case, but adam with scheduler also works
+        #self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=list(range(400,num_epochs,400)), gamma=0.1)
+        self.optimizer = optim.Adamax([{'params':self.D.parameters()}], lr=5e-4, weight_decay=1e-7, eps=1e-3) # optionally use this for 5000 points case, but adam with scheduler also works
 
     def train(self):
         self.D.train()
